4d7b340a37dde401e7ddcfeeeb9199b1f72a3369
John Marshall <John.Marshall@ssc-spc.gc.ca>
2016-03-28 20:40:03 +0000

    (HCRON-9) Remove unused function copytree (in library.py)

--- static/usr/lib/hcron/hcron/library.py
+++ static/usr/lib/hcron/hcron/library.py
@@ -178,34 +178,6 @@ def dir_walk(top, topdown=True, onerror=None, ignoreMatchFn=None):
     if not topdown:
         yield top, dirs, nondirs
 
-def copytree(src, dst, srcUid):
-    """This is a modified version of shutil.copytree (python v2.6).
-    * copier is owner
-    * no stat information retained
-    * symlinks are converted to (copyof) files
-    * an error raises an exception
-    """
-    from os import listdir, makedirs
-    from shutil import copy2
-
-    makedirs(dst)
-
-    try:
-        for name in listdir(src):
-            src2 = os.path.join(src, name)
-            dst2 = os.path.join(dst, name)
-
-            if os.path.isdir(src2):
-                copytree(src2, dst2)
-            else:
-                seteuid(srcUid)
-                x = open(src2, "r").read(CONFIG_MAX_EVENT_FILE_SIZE)
-                seteuid(0)
-                if len(x) < 5000:
-                    open(dst2, "w+").write(x)
-    except:
-        seteuid(0)
-
 def get_events_home(userName):
     """Returns the user-specific events/ directory path.
     """
