a5d1abdb29710f1720bae8b3aa3ef54f966b6824
John Marshall <John.Marshall2@canada.ca>
2019-08-21 21:48:40 -0400

    (HCRON-70) Update hcron-run frontend

--- static/usr/lib/hcron/hcron-run.py
+++ static/usr/lib/hcron/hcron-run.py
@@ -30,6 +30,7 @@ try:
 except:
     import StringIO
 import sys
+from sys import stderr
 import tarfile
 import time
 import traceback
@@ -43,8 +44,6 @@ from hcron import hcrontree
 from hcron.logger import *
 from hcron.library import date_to_bitmasks
 
-PROG_NAME = os.path.basename(sys.argv[0])
-
 # override
 def get_hcron_tree_filename(username, hostname):
     """Saved hcron tree file for user.
@@ -53,34 +52,36 @@ def get_hcron_tree_filename(username, hostname):
 hcrontree.get_hcron_tree_filename = get_hcron_tree_filename
 
 def print_usage():
+    d = {
+        "progname": os.path.basename(sys.argv[0])
+    }
     print """\
-usage: %s [<options>] <eventsdir> <startdatetime> <enddatetime>
-       %s -h|--help
+usage: %(progname)s [<options>] <eventsdir> <startdatetime> <enddatetime>
+       %(progname)s -h|--help
 
-Simulate events that would fire. Events are taken from the eventsdir
-directory and run over the period startdatetime to enddatetime (as
-YYYYMMDD[hhmm]).
+Simulate events that would execute. Events are taken from the <eventsdir>
+directory and run over the period <startdatetime> to <enddatetime>
+(specified as YYYYMMDD[hhmm]).
 
-Chained events will fire, but failovers will not.
+Chained events will execute, but failovers will not.
 
 Options:
--c <confpath>   Path of the hcron configuration file. Default is to
-                use the hcron-run.conf file provided with the
-                package (which is usually sufficient).
--d <delay>      Delay (in seconds) to use between subsequent
-                simulated datetimes for which there are events being
-                fired. Default is 0s (no delay).
+-c <confpath>       Path of the hcron configuration file. Default is to
+                    use the hcron-run.conf file provided with the package
+                    (which is usually sufficient).
+-d <delay>          Delay (in seconds) to use between subsequent simulated
+                    datetimes for which there are events being executed.
+                    Default is 0s (no delay).
 --show-all
 --show-email
 --show-event
-                Show email and/or event information as it would be
-                when an event fires. Early and late variable
-                substitution are applied.""" % (PROG_NAME, PROG_NAME)
+                    Show email and/or event information as it would be
+                    when an event executes. Early and late variable
+                    substitutions are applied.""" % d
 
 if __name__ == "__main__":
     whoami = pwd.getpwuid(os.getuid()).pw_name
 
-    args = sys.argv[1:]
     try:
         confpath = None
         delay = 0
@@ -88,26 +89,27 @@ if __name__ == "__main__":
         eventsdir = None
         startdatetime = None
 
+        args = sys.argv[1:]
         while args:
             arg = args.pop(0)
-            if arg in ["-h", "--help"]:
-                print_usage()
-                sys.exit(1)
-            elif arg == "-c" and args:
+            if arg == "-c" and args:
                 confpath = args.pop(0)
             elif arg == "-d" and args:
                 delay = max(0, float(args.pop(0)))
-            elif arg in ["--show-all"]:
+            elif arg == "--show-all":
                 globls.simulate_show_email = True
                 globls.simulate_show_event = True
-            elif arg in ["--show-email"]:
+            elif arg == "--show-email":
                 globls.simulate_show_email = True
-            elif arg in ["--show-event"]:
+            elif arg == "--show-event":
                 globls.simulate_show_event = True
             elif len(args) == 2:
                 eventsdir = os.path.realpath(arg)
                 startdatetime = datetime.strptime((args.pop(0)+"0000")[:12], "%Y%m%d%H%M")
                 enddatetime = datetime.strptime((args.pop(0)+"0000")[:12], "%Y%m%d%H%M")
+            elif arg in ["-h", "--help"]:
+                print_usage()
+                sys.exit(1)
             else:
                 raise Exception()
 
@@ -117,42 +119,45 @@ if __name__ == "__main__":
         raise
     except:
         #traceback.print_exc()
-        sys.stderr.write("error: bad/missing argument\n")
+        stderr.write("error: bad/missing argument\n")
         sys.exit(1)
 
-    #
-    # setup
-    #
-    globls.remote_execute_enabled = False
-    globls.email_notify_enabled = False
+    try:
+        globls.remote_execute_enabled = False
+        globls.email_notify_enabled = False
 
-    allowedUsers = [whoami]
+        allowedUsers = [whoami]
 
-    minute = timedelta(minutes=1)
-    now = startdatetime
-    globls.clock.set(now)
-    globls.simulate = True
-
-    if confpath:
-        constants.HCRON_CONFIG_PATH = os.path.realpath(confpath)
-    else:
-        etcdir = os.path.realpath("%s/../../etc" % os.path.dirname(sys.argv[0]))
-        constants.HCRON_CONFIG_PATH = os.path.join(etcdir, "hcron/hcron-run.conf")
-    constants.HOST_NAME = eventsdir.split("/")[-2]
-
-    globls.config = ConfigFile(constants.HCRON_CONFIG_PATH)
-    globls.config.get()["log_path"] = None
-    setup_logger()
-
-    globls.eventListList = EventListList(allowedUsers)
-
-    log_start()
-    while now < enddatetime:
-        hcronWeekday = now.isoweekday() % 7
-        datemasks = date_to_bitmasks(now.year, now.month, now.day, now.hour, now.minute, hcronWeekday)
-        events = globls.eventListList.test(datemasks)
-        for event in events:
-            handle_event(event, now)
-            time.sleep(delay)
-        now += minute
+        minute = timedelta(minutes=1)
+        now = startdatetime
         globls.clock.set(now)
+        globls.simulate = True
+
+        if confpath:
+            constants.HCRON_CONFIG_PATH = os.path.realpath(confpath)
+        else:
+            etcdir = os.path.realpath("%s/../../etc" % os.path.dirname(sys.argv[0]))
+            constants.HCRON_CONFIG_PATH = os.path.join(etcdir, "hcron/hcron-run.conf")
+        constants.HOST_NAME = eventsdir.split("/")[-2]
+
+        globls.config = ConfigFile(constants.HCRON_CONFIG_PATH)
+        globls.config.get()["log_path"] = None
+        setup_logger()
+
+        globls.eventListList = EventListList(allowedUsers)
+
+        log_start()
+        while now < enddatetime:
+            hcronWeekday = now.isoweekday() % 7
+            datemasks = date_to_bitmasks(now.year, now.month, now.day, now.hour, now.minute, hcronWeekday)
+            events = globls.eventListList.test(datemasks)
+            for event in events:
+                handle_event(event, now)
+                time.sleep(delay)
+            now += minute
+            globls.clock.set(now)
+    except:
+        stderr.write("error: unexpected situation\n")
+        sys.exit(1)
+
+    sys.exit(0)
