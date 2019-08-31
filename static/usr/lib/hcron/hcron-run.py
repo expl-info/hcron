e314b380395b1f92d934564c32aa24bb0ed4216d
John Marshall <John.Marshall2@canada.ca>
2019-08-31 07:31:12 -0400

    (HCRON-119) hcron-run to report error if start time after end time

--- static/usr/lib/hcron/hcron-run.py
+++ static/usr/lib/hcron/hcron-run.py
@@ -107,8 +107,13 @@ if __name__ == "__main__":
 
         if None in [enddatetime, eventsdir, startdatetime]:
             raise Exception()
+
         if not os.path.isdir(eventsdir):
             stderr.write("error: events directory (%s) not valid\n" % eventsdir)
+            sys.exit(1)
+
+        if startdatetime > enddatetime:
+            stderr.write("error: start time (%s) is after end time (%s)\n" % (startdatetime, enddatetime))
             sys.exit(1)
     except SystemExit:
         raise
